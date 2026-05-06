b52fe007e287fc54bcae56742d838a03b3c6b4b0
doubl <doubl@Gary>
2026-05-06 18:29:41 -0500

    Testing Eclipse git push from laptop.

--- SE433Project/src/edu/derryberry/ShoppingApplication.java
+++ SE433Project/src/edu/derryberry/ShoppingApplication.java
@@ -4,6 +4,7 @@ public class ShoppingApplication {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
+		System.out.println("Git push Eclipse laptop test! Yay!");
 		
 		// initialize Store
 		
