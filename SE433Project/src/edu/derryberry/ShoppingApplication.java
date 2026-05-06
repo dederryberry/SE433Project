4d2b75be1c6906b31c33f2e65b17e351c2f69149
dederryberry <dustin.e.derryberry@gmail.com>
2026-05-06 18:44:05 -0500

    Test 2

--- SE433Project/src/edu/derryberry/ShoppingApplication.java
+++ SE433Project/src/edu/derryberry/ShoppingApplication.java
@@ -5,6 +5,7 @@ public class ShoppingApplication {
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
 		System.out.println("Git push Eclipse laptop test! Yay!");
+		System.out.println("Git push Eclipse laptop test 2! Huzzah.");
 		
 		// initialize Store
 		
